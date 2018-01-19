f8210b4e28ce1f1f9ccf608050b617ea9f6b64db
Alireza Nourian <az.nourian@gmail.com>
2018-01-19 22:28:38 +0330

    update app port

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 ps -ef | grep openiesearch | awk '{print $2}' | xargs kill -9
 
-gunicorn --bind=0.0.0.0:8000 --workers=1 app --timeout 10 --name openiesearch --log-file -
+gunicorn --bind=0.0.0.0:9000 --workers=1 app --timeout 10 --name openiesearch --log-file -
+
